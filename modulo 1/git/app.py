9282e633b2930268d2732a1b408c3213a6fb3c58
tjota27 <edisontjota@gmail.comn>
2025-10-03 21:09:54 -0500

    programador tanqueno practica

--- modulo 1/git/app.py
+++ modulo 1/git/app.py
@@ -3,8 +3,5 @@ print("Programacion III")
 
 
 print("Hola desde la UTE")
-print("Hola desde la UTE")
 
-print("Yo soy un programador")
-
-print("Cambio para guardar el stash")
+print("Hola programador Tanqueno")
